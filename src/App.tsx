c91711cbc811bc7a74d56e9b06762e81f9db8e8c
Anderson Menezes <anderson.stifip@gmail.com>
2024-02-07 17:06:22 -0300

    feat: finalização de estilização do app

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import logo from './assets/logo-nwl-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: 'Grave uma nota em áudio que será convertida para texto automaticamente.',
+}
+
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
       <img
         src={logo}
         alt="NLW Expert"
       />
-      <form className="w-full mt-6">
+      <form className="mt-6 w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
@@ -19,13 +24,10 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
     </div>
   )
