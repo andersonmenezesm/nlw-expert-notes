bf791795d9a2128563aafc7775cff62dc3ac82ad
Anderson Menezes <anderson.stifip@gmail.com>
2024-02-08 19:06:54 -0300

    fix: name de import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'sonner'
-import { App } from './app'
 
+import { App } from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
